9bc80be2fcdca8e96d548be86f03a86852436b4b
Robert Ying <me@robertying.com>
2023-12-20 23:11:09 -0800

    day21

---
+++ src/day21.rs
@@ -0,0 +1,130 @@
+use crate::prelude::*;
+
+pub fn part1(input: &str, target: i64) -> i64 {
+    let g = parse_char_grid(input);
+
+    let (start_pos, _) = g.iter().find(|(_, c)| **c == 'S').unwrap();
+
+    let mut queue = VecDeque::new();
+    queue.push_back((*start_pos, target));
+
+    let mut visited: DefaultHashMap<i64, HashSet<(i64, i64)>> = DefaultHashMap::default();
+
+    while let Some((n, remaining)) = queue.pop_front() {
+        if visited[remaining].contains(&n) {
+            continue;
+        }
+        visited[remaining].insert(n);
+        if remaining == 0 {
+            continue;
+        }
+
+        for x in adjacents(n, FOUR_WAY) {
+            if (g[x] == '.' || g[x] == 'S') && !visited[remaining - 1].contains(&x) {
+                queue.push_back((x, remaining - 1));
+            }
+        }
+    }
+
+    visited[0].len() as i64
+}
+
+pub fn part2(input: &str) -> i64 {
+    let g = parse_char_grid(input);
+    let (i_bounds, j_bounds) = get_grid_bounds(&g);
+
+    let (start_pos, _) = g.iter().find(|(_, c)| **c == 'S').unwrap();
+
+    let mut queue = VecDeque::new();
+    queue.push_back((*start_pos, 0));
+
+    let mut visited: DefaultHashMap<i64, HashSet<(i64, i64)>> = DefaultHashMap::default();
+    let mut visited2 = HashSet::new();
+    let mut counts = vec![];
+
+    let target: i64 = 26501365;
+
+    while let Some((n, step)) = queue.pop_front() {
+        if visited[step].contains(&n) {
+            continue;
+        }
+        visited[step].insert(n);
+
+        // if we got to `step`, we've finished `step-1`. Do some cleanup
+        if !visited2.contains(&(step - 1)) {
+            let prev_len = visited[step - 1].len() as i64;
+            if ((step - 1) - i_bounds.end / 2) % i_bounds.end == 0 {
+                counts.push(prev_len);
+
+                eprintln!("counts: {:?}", counts);
+                if counts.len() == 3 {
+                    let dcounts = counts.windows(2).map(|w| w[1] - w[0]).collect::<Vec<_>>();
+                    eprintln!("\tdcounts: {:?}", dcounts);
+                    let ddcounts = dcounts.windows(2).map(|w| w[1] - w[0]).collect::<Vec<_>>();
+                    eprintln!("\tddcounts: {:?}", ddcounts);
+
+                    let a = ddcounts[0];
+                    let b = dcounts[0];
+                    let c = counts[0];
+
+                    let x = (target - i_bounds.end / 2) / i_bounds.end;
+                    eprintln!("x = {}", x);
+                    eprintln!("a = {}, b = {}, c = {}", a, b, c);
+
+                    // This is polynomial interpretation using Newton's
+                    // interpolating polynomial
+                    return x * (x - 1) * a / 2 + x * b + c;
+                }
+            }
+            visited.remove(&(step - 1));
+            visited2.insert(step - 1);
+        }
+
+        if step == target {
+            continue;
+        }
+
+        for x in adjacents(n, FOUR_WAY) {
+            let xx = (x.0.rem_euclid(i_bounds.end), x.1.rem_euclid(j_bounds.end));
+
+            if (g[xx] == '.' || g[xx] == 'S') && !visited[step + 1].contains(&x) {
+                queue.push_back((x, step + 1));
+            }
+        }
+    }
+    print_char_grid(&g);
+
+    visited[target].len() as i64
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = r#"...........
+.....###.#.
+.###.##..#.
+..#.#...#..
+....#.#....
+.##..S####.
+.##..#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+..........."#;
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(part1(EXAMPLE, 6), 16);
+    }
+
+    #[test]
+    fn part1_input() {
+        assert_eq!(part1(&read_day_input(std::module_path!()), 64), 3578);
+    }
+
+    #[test]
+    fn part2_input() {
+        assert_eq!(part2(&read_day_input(std::module_path!())), 594115391548176);
+    }
+}
